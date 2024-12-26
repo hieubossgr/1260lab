592339ea21b7c6e1fc55980e47075a7ad38b4947
Hieunt <hieunt@hblab.vn>
2024-12-26 16:28:00 +0700

    update key

--- local-modules/key_pair/kp.tf
+++ local-modules/key_pair/kp.tf
@@ -14,13 +14,17 @@ resource "aws_key_pair" "key_pair" {
   public_key = tls_private_key.key.public_key_openssh
 }
 
+resource "tls_private_key" "bastion_key" {
+  algorithm = "RSA"
+}
+
 resource "local_sensitive_file" "bastion_private_key" {
   filename        = "${path.module}/${var.project_name}-${var.env}-bastion.pem"
-  content         = tls_private_key.key.private_key_pem
+  content         = tls_private_key.bastion_key.private_key_pem
   file_permission = "0400"
 }
 
 resource "aws_key_pair" "bastion_key_pair" {
   key_name   = "${var.project_name}-${var.env}-bastion"
-  public_key = tls_private_key.key.public_key_openssh
+  public_key = tls_private_key.bastion_key.public_key_openssh
 }
